fb083c87d04c456fa4074927b569a6b728cfc593
Laurens Groeneveld <laurens.groeneveld@infi.nl>
2019-03-04 16:54:13 +0100

    Random player 'messages'

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -40,4 +40,66 @@ export class DashboardComponent implements OnInit, OnDestroy {
     this.destroyed$.next();
   }
 
+  private winnerTexts = [
+    'Winner Takes All',
+    'The Champion',
+    'Prime Target',
+    'Nice View Up Here',
+    'No. 1',
+    'Lonely Here',
+  ]
+
+  private runnerUpTexts = [
+    'Almost There',
+    'Best Of The Rest',
+    'Almost Top',
+    'Not Too Bad',
+    'Eternal No. 2',
+  ]
+
+  private otherTexts = [
+    '',
+    'Way To Go',
+    'Up!',
+    'Made The List',
+    'Not So Bad',
+    'Got Them Aliens',
+    'Pretty Good',
+    'Dope',
+    'Cool',
+    'It\'s Something',
+    'Loading...',
+    'Not Yet Lost',
+    'Look At Me',
+    'Need Coffee',
+    'Fire And Fury',
+    'Good Shot',
+    'Worth It',
+    'Famous Now',
+    'There We Go',
+    'Still Awesome',
+    'On The Dark Side',
+    'Improving!',
+    'Witty Joke Here',
+    'Star Destroyer',
+    'Prospering',
+    'Living The Life',
+    'Alert All Commands',
+    'Deploy The Fleet',
+  ]
+
+  randomText(position: number) {
+    if (position == 0)
+    {
+      return this.winnerTexts[Math.floor(Math.random() * this.winnerTexts.length)];
+    }
+    
+    if (position == 1)
+    {
+      return this.runnerUpTexts[Math.floor(Math.random() * this.runnerUpTexts.length)];
+    }
+    
+    return this.otherTexts[Math.floor(Math.random() * this.otherTexts.length)];
+  }
+
 }
