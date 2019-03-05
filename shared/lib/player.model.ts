cd43acc93b6905150722b9989e0f99fbf3b4679b
PeerHenry <peer@infi.nl>
2019-03-05 14:30:40 +0100

    PlayerSettings

--- shared/lib/player.model.ts
+++ shared/lib/player.model.ts
@@ -5,5 +5,16 @@ export class Player {
         public kills: number,
         public won: boolean,
         public time: number,
+        public settings: PlayerSettings,
+    ) {}
+}
+
+export class PlayerSettings {
+    constructor(
+        public shipShields: number,
+        public defenseThickness: number,
+        public defenseWidth: number,
+        public firePower: number,
+        public dodgeChance: number,
     ) {}
 }
